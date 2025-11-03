4c204ffae6cda0293302d59e4863be0fe0e6817f
lLorran408 <elorran321@gmail.com>
2025-11-03 12:50:54 -0300

    fix

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXheeGRlpaql-toRfZjZLa3Ufp5Ah1Jeo",
+  authDomain: "fronender-3e1a0.firebaseapp.com",
+  projectId: "fronender-3e1a0",
+  storageBucket: "fronender-3e1a0.firebasestorage.app",
+  messagingSenderId: "701636019805",
+  appId: "1:701636019805:web:62048c5f964919be62afb4",
+  measurementId: "G-BZ3R0RV715"
+};
