9818a38827ef5320ab4bc45efba431b06fef95c5
lLorran408 <elorran321@gmail.com>
2025-11-12 09:06:41 -0300

    fix

--- experimentos.sql
+++ experimentos.sql
@@ -1,27 +1,45 @@
--- Exemplos de operação do banco de dados SQLite
+-- ------------------------------------------------ --
+-- Script SQL para experimentações com comandos SQL --
+-- ------------------------------------------------ --
 
--- Apaga as tabelas caso elas já existam
--- Devtime → Isso não deve ser feito com o banco de dados de produção
+-- Apaga todas as tabelas do banco de dados
+-- ATENÇÃO! Só use isso no banco de dados de desenvolvimento
 DROP TABLE IF EXISTS pads;
 DROP TABLE IF EXISTS owners;
 
---Cria a tabela "owners"
+-- Cria a tabela "owners"
+-- Precisa ser criada primeiro porque "pads" depende dela
 CREATE TABLE owners (
-	-- Chave primária
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	uid TEXT UNIQUE, 
-	displayName TEXT,
-	email TEXT UNIQUE,
-	photoURL TEXT,
-	createdAt t DEFAULT CURRENT_TIMESTAMP,
-	lastLoginAt TIMASTAMP DEFAULT CURRENT_TIMESTAMP,
-	status TEXT NOT NULL DEFAULT 'ON' CHECK (status IN ('ON', 'OFF', 'DEL')),
-	-- "metadata" é reservado para uso futuro
-	metadata TEXT
+	own_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	own_uid TEXT UNIQUE NOT NULL,
+	own_display_name TEXT,
+	own_email TEXT UNIQUE NOT NULL,
+	-- URL da foto do usuário
+	own_photo_url TEXT,
+	-- 'DEFAULT' significa que vai ser preechido automaticamente
+	own_created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+	own_last_login_at TEXT DEFAULT CURRENT_TIMESTAMP,
+	own_status TEXT NOT NULL DEFAULT 'ON' CHECK (own_status IN ('ON', 'OFF', 'DEL')),
+	-- Reservado para uso futuro
+	own_metadata TEXT
 );
 
-
-
-
-
-
+-- Insere alguns registros em "owners" para teste
+INSERT INTO owners (
+	own_uid,
+	own_display_name,
+	own_email,
+	own_photo_url
+) VALUES (
+	'92U33H8E839H',
+	'Joca da Silva',
+	'jocasilva@gmail.com',
+	'img/fotojoca.jpg'
+), (
+	'96u0108319hd011',
+	'Lorran eduardo',
+	'elorran321@gmai.com',
+	'img/fotolorran.jpg'
+);
+-- Lista todos os registros de 'owners'
+SELECT * FROM owners
